5626144af8db86e6fbcaa06c1a6879f80fef5900
Lucas Spencer <lucas.spencer14@gmail.com>
2022-05-17 13:50:26 -0600

    first

---
+++ hello.py
@@ -0,0 +1,42 @@
+# FIZZBUZZ
+# for x in range(101):
+#     if (x % 3 == 0) and (x % 5 == 0):
+#         print("%s - fizzbuzz!" % x)
+#     elif (x % 3 == 0):
+#         print("%s - fizz" % x)
+#     elif (x % 5 == 0):
+#         print("%s - buzz" % x)
+#     else:
+#         print(x)
+
+# FUNCTIONS
+# def mather(num1, num2):
+#     print(num1 * num2)
+
+
+# mather(2, 3)
+
+# IMPORT FILES
+# import function
+
+# print(function.namer('Lucas'))
+
+# CLASSES
+# class Square:
+#     def __init__(self, sideLength):
+#         self.sideLength = sideLength
+
+#     def area(self):
+#         return self.sideLength * self.sideLength
+
+#     def perimeter(self):
+#         return self.sideLength * 4
+
+#     def report(self):
+#         print("Side Length: %s" % self.sideLength)
+#         print("Area: %s" % self.area())
+#         print("Perimeter: %s" % self.perimeter())
+
+
+# mySquare = Square(10)
+# mySquare.report()
